c7bc8e72974ab5291727bbc0941e9e6a9eb8b823
siddharth rout <srout5096@gmail.com>
2021-07-06 08:26:07 +0530

    Final change

---
+++ build/precache-manifest.bb6c52b987255a09c9a94ac4f52ac01f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e94de58e03bd764578e03d2e0959fbdc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e3f26b38b91ce6a677e4",
+    "url": "/static/css/main.e4785142.chunk.css"
+  },
+  {
+    "revision": "3d411b0f9ac1d5a2671d",
+    "url": "/static/js/2.d3c0597b.chunk.js"
+  },
+  {
+    "revision": "bfd8682dcfd7143d7e9c5369b9c3f456",
+    "url": "/static/js/2.d3c0597b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3f26b38b91ce6a677e4",
+    "url": "/static/js/main.1f862eb4.chunk.js"
+  },
+  {
+    "revision": "6fc4f2c3fd5ae8d05fd1",
+    "url": "/static/js/runtime-main.4ba1596e.js"
+  },
+  {
+    "revision": "6a834649522a287b4cf8d59fdd0aef77",
+    "url": "/static/media/google-logo-9827.6a834649.png"
+  }
+]);
